9d93c9b9c4d0521664b1a091de0fd31111d4be80
Robsen01 <rob.nau833@googlemail.com>
2024-03-13 21:52:38 +0100

    add head

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return(
+        <div>
+            <head>
+                <title>My Portfolio Page</title>
+            </head>
+        </div>
+    )
+}
+
+export default Header
