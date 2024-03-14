8eff907cb227dd5e6c80707dfa4a2afd947c0cf4
Robsen01 <rob.nau833@googlemail.com>
2024-03-14 01:09:34 +0100

    Adds Body with Background image

---
+++ src/components/Body.jsx
@@ -0,0 +1,12 @@
+import './Body.css'
+import Background from '../assets/Background.png'
+
+function Body() {
+    return(
+        <div class="body">
+            <img src={Background} alt="BG" width="100%" />
+        </div>
+    )
+}
+
+export default Body
